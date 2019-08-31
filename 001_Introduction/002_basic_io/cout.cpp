17c1825024f46f363d1d3a5e8a4000e1f2ed62aa
Jack Ngo <jngo@example.com>
2019-08-31 11:28:06 -0400

    Basic io

---
+++ 001_Introduction/002_basic_io/cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Jack Ngo";
+    cout << "Hello "
+         << name << ","
+         << "cout" << endl;
+
+    cout << "I Love C++ programming!" << endl;
+    return 0;
+}
