443df8fa28dc872733ee1ea12eb497d91d5b7839
Jack Ngo <jngo@example.com>
2019-10-05 16:45:27 -0400

    signed integrals

---
+++ 003_data_types/001_categories/002_integral/signed_climits.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    cout << "short: " << endl;
+    cout << "SHRT_MIN: " << SHRT_MIN << endl;
+    cout << "SHRT_MAX: " << SHRT_MAX << endl;
+
+    cout << "int: " << endl;
+    cout << "INT_MIN: " << INT_MIN << endl;
+    cout << "INT_MAX: " << INT_MAX << endl;
+
+    cout << "long: " << endl;
+    cout << "LONG_MIN: " << LONG_MIN << endl;
+    cout << "LONG_MAX: " << LONG_MAX << endl;
+
+    cout << "long long: " << endl;
+    cout << "LONG LONG_MIN: " << LLONG_MIN << endl;
+    cout << "LONG LONG_MAX: " << LLONG_MAX << endl;
+    return 0;
+}
---
+++ 003_data_types/001_categories/002_integral/signed_sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short a;
+    int b;
+    long c;
+    long long d;
+
+    cout << "Size of short: " << sizeof(short) << endl;
+    cout << "Size of int: " << sizeof(int) << endl;
+    cout << "Size of long: " << sizeof(long) << endl;
+    cout << "Size of long long: " << sizeof(long long) << endl;
+}
